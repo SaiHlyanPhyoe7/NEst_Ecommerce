6c464ee965135c4853d72d2871a122c06b1dc443
SaiHlyanPhyoe <saihlyan.phyoe@kbzbank.com>
2023-05-09 16:26:07 +0630

    hotfix: swagger ui not show

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,17 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-import { resolve } from 'path';
-import { writeFileSync } from 'fs';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  // Serve Swagger UI files from the public folder
+  app.use('/swagger-ui', express.static(join(__dirname, '..', 'public')));
 
   const config = new DocumentBuilder()
     .setTitle('Ecommerce API SHP')
