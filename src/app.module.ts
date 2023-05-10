c8b8bf3bbd424ee3fc37e1add8ef715b2e904dfd
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-05-10 16:00:58 +0630

    fix: delete unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { ProductsModule } from './products/products.module';
 import { CustomersModule } from './customers/customers.module';
 import { OrdersModule } from './orders/orders.module';
 import { ReviewsModule } from './reviews/reviews.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
   imports: [
@@ -16,10 +14,6 @@ import { join } from 'path';
     CustomersModule,
     OrdersModule,
     ReviewsModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'swagger-static'),
-      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/api',
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
